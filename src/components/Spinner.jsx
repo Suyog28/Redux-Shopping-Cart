f166c8ec3a26f0b4194e045dbc7e5a6a1b100b64
Suyog28 <suyogvmuley@gmail.com>
2023-04-23 20:41:45 +0530

    Shopping Cart Redux Project Completed

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+    return (
+        <div className='spinner flex flex-col gap-10 justify-center items-center'>Loading</div>
+    )
+}
+
+export default Spinner
